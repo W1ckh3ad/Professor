14ef6d1769f3a10d703e7468c16aad18c8137c15
W1ckh3ad <69201877+W1ckh3ad@users.noreply.github.com>
2020-08-04 16:37:08 +0200

    UseClasses

--- src/App.java
+++ src/App.java
@@ -1,8 +1,7 @@
-import Classes.*;
+import Demo.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        var p1 = new Professor("Franz");
-        System.out.println(p1.toString());
+       Test.Professor();
     }
 }
---
+++ src/Demo/Test.java
@@ -0,0 +1,10 @@
+package Demo;
+import Classes.*;
+
+public  class Test {
+    
+    public static void Professor() {
+        var p1 = new Professor("Franz");
+        System.out.println(p1.toString());
+    }
+}
