b2409da1dfc8e52ce593eebcfc356e9d03841266
W1ckh3ad <69201877+W1ckh3ad@users.noreply.github.com>
2020-08-04 16:15:50 +0200

    Test

---
+++ src/Classes/Test.java
@@ -0,0 +1,37 @@
+package Classes;
+
+public class Test {
+    private int ID;
+    private String Owner;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(int owner) {
+        Owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Test [ID=" + ID + ", Owner=" + Owner + "]";
+    }
+
+    public Test(int iD, String owner) {
+        ID = iD;
+        Owner = owner;
+    }
+
+    public Test(String owner) {
+        Owner = owner;
+    }
+
+}
